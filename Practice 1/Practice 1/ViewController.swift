ac443d921a3c7017d967cacba20536796522b1ff
Marcus Paze <yingting1996@gmail.com>
2016-08-10 17:38:35 +0800

    moving block and red background

--- Practice 1/Practice 1/ViewController.swift
+++ Practice 1/Practice 1/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view1: UIView!
+    @IBAction func leftAction(sender: AnyObject) {
+        print("click left")
+        view1.frame.origin.x -= 10
+    }
+    @IBAction func upAction(sender: AnyObject) {
+        print("click up")
+        view1.frame.origin.y -= 10
+    }
+    @IBAction func downAction(sender: AnyObject) {
+        print("click down")
+        view1.frame.origin.y += 10
+    }
+    @IBAction func rightAction(sender: AnyObject) {
+        
+        print("click right")
+        view1.frame.origin.x += 10
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.redColor()
+
     }
 
     override func didReceiveMemoryWarning() {
