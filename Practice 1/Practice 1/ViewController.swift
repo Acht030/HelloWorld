4f96d049e3131179984cdf08058d52fe6bf55a6b
Marcus Paze <yingting1996@gmail.com>
2016-08-10 16:54:42 +0800

    initial project commit

---
+++ Practice 1/Practice 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice 1
+//
+//  Created by Marcus Paze on 8/10/16.
+//  Copyright © 2016 Marcus Paze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
